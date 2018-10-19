fc7379328766628664eb027357d2323d1648970e
cjw <1828343424@qq.com>
2018-10-19 17:08:07 +0800

    版本v3.0

--- src/main/java/com/baizhi/test/DemoTest.java
+++ src/main/java/com/baizhi/test/DemoTest.java
@@ -4,5 +4,6 @@ public class DemoTest {
     public static void main(String[] args) {
         System.out.println("文个nb");
         System.out.println("刘世勋智障");
+        System.out.println("哈哈");
     }
 }
